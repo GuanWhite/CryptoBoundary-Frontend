dafc55c059f95a1834fc96a02683bc4a915d18d7
white <3488337866@qq.com>
2025-04-06 23:24:02 +0800

    developing login page

--- src/pages/loginPage/LoginPage.jsx
+++ src/pages/loginPage/LoginPage.jsx
@@ -2,121 +2,206 @@ import "./LoginPage.less";
 import StartButton from "../../components/StartButton/StartButton";
 import { MailOutlined, LockOutlined, GoogleOutlined, FacebookOutlined, GithubOutlined, LinkedinOutlined } from "@ant-design/icons";
 import { Input } from 'antd';
+import React, { useState } from 'react';
 function Login() {
-  const container = document.querySelector('.container')
-const registerBtn = document.querySelector('.register-btn')
-const loginBtn = document.querySelector('.login-btn')
+  //   const container = document.querySelector('.container')
+  // const registerBtn = document.querySelector('.register-btn')
+  // const loginBtn = document.querySelector('.login-btn')
 
-registerBtn.addEventListener('click', () => {
-  container.classList.add('active')
-})
+  // registerBtn.addEventListener('click', () => {
+  //   container.classList.add('active')
+  // })
 
-loginBtn.addEventListener('click', () => {
-  container.classList.remove('active')
-})
+  // loginBtn.addEventListener('click', () => {
+  //   container.classList.remove('active')
+  // })
+  // return (
+  //   <>
+
+  //     {/* <div className="login-back-box">
+  //       <div className="login-container">
+  //         <div className="form-box login">
+  //           <form className="login-form" action="">
+  //             <h1 className="login-h1">Login</h1>
+  //             <div className="input-box">
+  //               <Input className="input-input" placeholder="Email" />
+  //               <MailOutlined className="input-icon" />
+  //             </div>
+  //             <div className="input-box">
+  //               <Input.Password className="input-input" placeholder="Password" />
+  //               <LockOutlined className="input-icon" />
+  //             </div>
+  //             <div className="forgot-link">
+  //               <a href="#">Forget Password?</a>
+  //             </div>
+  //             <button type="submit" className="login-btn">Login</button>
+  //             <p>or login with social platforms</p>
+  //             <div className="social-icons">
+  //               <a href="#"><GoogleOutlined /></a>
+  //               <a href="#"><FacebookOutlined /></a>
+  //               <a href="#"><GithubOutlined /></a>
+  //               <a href="#"><LinkedinOutlined /></a>
+  //             </div>
+  //           </form>
+  //         </div>
+  //       </div>
+  //     </div> */}
+
+  //     {/* <div class="container">
+  //       <div class="form-box login">
+  //         <form action="">
+  //           <h1>Login</h1>
+  //           <div class="input-box">
+  //             <input type="text" placeholder="Username" required />
+  //               <i class="bx bxs-user"></i>
+  //           </div>
+  //           <div class="input-box">
+  //             <input type="password" placeholder="Password" required />
+  //               <i class="bx bxs-lock-alt"></i>
+  //           </div>
+  //           <div class="forgot-link">
+  //             <a href="#">Forgot Password?</a>
+  //           </div>
+  //           <button type="submit" class="btn">Login</button>
+  //           <p>or login with social platforms</p>
+  //           <div class="social-icons">
+  //             <a href="#"><i class="bx bxl-google"></i></a>
+  //             <a href="#"><i class="bx bxl-facebook"></i></a>
+  //             <a href="#"><i class="bx bxl-github"></i></a>
+  //             <a href="#"><i class="bx bxl-linkedin"></i></a>
+  //           </div>
+  //         </form>
+  //       </div>
+
+  //       <div class="form-box register">
+  //         <form action="">
+  //           <h1>Registration</h1>
+  //           <div class="input-box">
+  //             <input type="text" placeholder="Username" required />
+  //               <i class="bx bxs-user"></i>
+  //           </div>
+  //           <div class="input-box">
+  //             <input type="email" placeholder="Email" required />
+  //               <i class="bx bxs-envelope"></i>
+  //           </div>
+  //           <div class="input-box">
+  //             <input type="password" placeholder="Password" required />
+  //               <i class="bx bxs-lock-alt"></i>
+  //           </div>
+  //           <div class="forgot-link">
+  //             <a href="#">Forgot Password?</a>
+  //           </div>
+  //           <button type="submit" class="btn">Register</button>
+  //           <p>or register with social platforms</p>
+  //           <div class="social-icons">
+  //             <a href="#"><i class="bx bxl-google"></i></a>
+  //             <a href="#"><i class="bx bxl-facebook"></i></a>
+  //             <a href="#"><i class="bx bxl-github"></i></a>
+  //             <a href="#"><i class="bx bxl-linkedin"></i></a>
+  //           </div>
+  //         </form>
+  //       </div>
+
+  //       <div class="toggle-box">
+  //         <div class="toggle-panel toggle-left">
+  //           <h1>Hello, Welcome!</h1>
+  //           <p>Don's have an account?</p>
+  //           <button class="btn register-btn">Register</button>
+  //         </div>
+
+  //         <div class="toggle-panel toggle-right">
+  //           <h1>Welcome Back!</h1>
+  //           <p>Already have an account?</p>
+  //           <button class="btn login-btn">Login</button>
+  //         </div>
+  //       </div>
+  //     </div> */}
+
+  //   </>
+  // );
+
+  const [isActive, setIsActive] = useState(false);
+
+  const handleRegisterClick = () => {
+    setIsActive(true);
+  };
+
+  const handleLoginClick = () => {
+    setIsActive(false);
+  };
+
   return (
-    <>
-
-      {/* <div className="login-back-box">
-        <div className="login-container">
-          <div className="form-box login">
-            <form className="login-form" action="">
-              <h1 className="login-h1">Login</h1>
-              <div className="input-box">
-                <Input className="input-input" placeholder="Email" />
-                <MailOutlined className="input-icon" />
-              </div>
-              <div className="input-box">
-                <Input.Password className="input-input" placeholder="Password" />
-                <LockOutlined className="input-icon" />
-              </div>
-              <div className="forgot-link">
-                <a href="#">Forget Password?</a>
-              </div>
-              <button type="submit" className="login-btn">Login</button>
-              <p>or login with social platforms</p>
-              <div className="social-icons">
-                <a href="#"><GoogleOutlined /></a>
-                <a href="#"><FacebookOutlined /></a>
-                <a href="#"><GithubOutlined /></a>
-                <a href="#"><LinkedinOutlined /></a>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div> */}
-
-      <div class="container">
-        <div class="form-box login">
+    <div className="auth-wrapper">
+      <div className={`container ${isActive ? 'active' : ''}`}>
+        <div className="form-box login">
           <form action="">
             <h1>Login</h1>
-            <div class="input-box">
+            <div className="input-box">
               <input type="text" placeholder="Username" required />
-                <i class="bx bxs-user"></i>
+              <i className="bx bxs-user"></i>
             </div>
-            <div class="input-box">
+            <div className="input-box">
               <input type="password" placeholder="Password" required />
-                <i class="bx bxs-lock-alt"></i>
+              <i className="bx bxs-lock-alt"></i>
             </div>
-            <div class="forgot-link">
+            <div className="forgot-link">
               <a href="#">Forgot Password?</a>
             </div>
-            <button type="submit" class="btn">Login</button>
+            <button type="submit" className="btn">Login</button>
             <p>or login with social platforms</p>
-            <div class="social-icons">
-              <a href="#"><i class="bx bxl-google"></i></a>
-              <a href="#"><i class="bx bxl-facebook"></i></a>
-              <a href="#"><i class="bx bxl-github"></i></a>
-              <a href="#"><i class="bx bxl-linkedin"></i></a>
+            <div className="social-icons">
+              <a href="#"><i className="bx bxl-google"></i></a>
+              <a href="#"><i className="bx bxl-facebook"></i></a>
+              <a href="#"><i className="bx bxl-github"></i></a>
+              <a href="#"><i className="bx bxl-linkedin"></i></a>
             </div>
           </form>
         </div>
 
-        <div class="form-box register">
+        <div className="form-box register">
           <form action="">
             <h1>Registration</h1>
-            <div class="input-box">
+            <div className="input-box">
               <input type="text" placeholder="Username" required />
-                <i class="bx bxs-user"></i>
+              <i className="bx bxs-user"></i>
             </div>
-            <div class="input-box">
+            <div className="input-box">
               <input type="email" placeholder="Email" required />
-                <i class="bx bxs-envelope"></i>
+              <i className="bx bxs-envelope"></i>
             </div>
-            <div class="input-box">
+            <div className="input-box">
               <input type="password" placeholder="Password" required />
-                <i class="bx bxs-lock-alt"></i>
+              <i className="bx bxs-lock-alt"></i>
             </div>
-            <div class="forgot-link">
+            <div className="forgot-link">
               <a href="#">Forgot Password?</a>
             </div>
-            <button type="submit" class="btn">Register</button>
+            <button type="submit" className="btn">Register</button>
             <p>or register with social platforms</p>
-            <div class="social-icons">
-              <a href="#"><i class="bx bxl-google"></i></a>
-              <a href="#"><i class="bx bxl-facebook"></i></a>
-              <a href="#"><i class="bx bxl-github"></i></a>
-              <a href="#"><i class="bx bxl-linkedin"></i></a>
+            <div className="social-icons">
+              <a href="#"><i className="bx bxl-google"></i></a>
+              <a href="#"><i className="bx bxl-facebook"></i></a>
+              <a href="#"><i className="bx bxl-github"></i></a>
+              <a href="#"><i className="bx bxl-linkedin"></i></a>
             </div>
           </form>
         </div>
 
-        <div class="toggle-box">
-          <div class="toggle-panel toggle-left">
+        <div className="toggle-box">
+          <div className="toggle-panel toggle-left">
             <h1>Hello, Welcome!</h1>
-            <p>Don's have an account?</p>
-            <button class="btn register-btn">Register</button>
+            <p>Don't have an account?</p>
+            <button className="btn register-btn" onClick={handleRegisterClick}>Register</button>
           </div>
 
-          <div class="toggle-panel toggle-right">
+          <div className="toggle-panel toggle-right">
             <h1>Welcome Back!</h1>
             <p>Already have an account?</p>
-            <button class="btn login-btn">Login</button>
+            <button className="btn login-btn" onClick={handleLoginClick}>Login</button>
           </div>
         </div>
       </div>
-
-    </>
+    </div>
   );
 }
 export default Login;
