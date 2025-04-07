39bffcffa8a9f4aa6d9ceb5c9fd144efd5d5e0ac
white <3488337866@qq.com>
2025-04-07 20:10:59 +0800

    finish login page

--- src/pages/loginPage/LoginPage.jsx
+++ src/pages/loginPage/LoginPage.jsx
@@ -1,6 +1,6 @@
 import "./LoginPage.less";
 import StartButton from "../../components/StartButton/StartButton";
-import { MailOutlined, LockOutlined, GoogleOutlined, FacebookOutlined, GithubOutlined, LinkedinOutlined } from "@ant-design/icons";
+import { MailOutlined, LockOutlined, UserOutlined, GoogleOutlined, FacebookOutlined, GithubOutlined, LinkedinOutlined } from "@ant-design/icons";
 import { Input } from 'antd';
 import React, { useState } from 'react';
 function Login() {
@@ -36,12 +36,12 @@ function Login() {
   //             </div>
   //             <button type="submit" className="login-btn">Login</button>
   //             <p>or login with social platforms</p>
-  //             <div className="social-icons">
-  //               <a href="#"><GoogleOutlined /></a>
-  //               <a href="#"><FacebookOutlined /></a>
-  //               <a href="#"><GithubOutlined /></a>
-  //               <a href="#"><LinkedinOutlined /></a>
-  //             </div>
+  // <div className="social-icons">
+  //   <a href="#"><GoogleOutlined /></a>
+  //   <a href="#"><FacebookOutlined /></a>
+  //   <a href="#"><GithubOutlined /></a>
+  //   <a href="#"><LinkedinOutlined /></a>
+  // </div>
   //           </form>
   //         </div>
   //       </div>
@@ -121,6 +121,52 @@ function Login() {
   // );
 
   const [isActive, setIsActive] = useState(false);
+  const [time, setTime] = useState(60);
+  const [isShowCode, setIsShowCode] = useState(false);
+
+  // 将参数传给后端发送验证码或前端调用其他发送验证码的api
+  const sendEmailCode = async (params) => {
+    // const res = await request.post('/user/sendEmailCode', params);
+    // return res;
+    return {
+      responseCode: '000000', // 模拟成功响应
+      responseMsg: "Verification code sent successfully!",
+    };
+  };
+
+  // 发送邮箱验证码
+  const handleSendEmailCode = async () => {
+    // const fileds = await form.validateFields(['account', 'email']);
+
+    if (isShowCode) { // 倒计时未结束,不能重复点击
+      return;
+    }
+    setIsShowCode(true);
+
+    // 倒计时
+    const active = setInterval(() => {
+      setTime((preSecond) => {
+        if (preSecond <= 1) {
+          setIsShowCode(false);
+          clearInterval(active);
+          // 重置秒数
+          return 60;
+        }
+        return preSecond - 1;
+      });
+    }, 1000);
+
+
+    // 模拟获取到的邮箱
+    userEmail = "123@qq.com";
+    // 将邮箱作为参数传入
+    const res = await sendEmailCode(userEmail);
+    if (res.responseCode === '000000') {
+      notification.success({
+        message: '发送成功,请填写收到的验证码',
+      });
+    }
+  };
 
   const handleRegisterClick = () => {
     setIsActive(true);
@@ -137,12 +183,14 @@ function Login() {
           <form action="">
             <h1>Login</h1>
             <div className="input-box">
-              <input type="text" placeholder="Username" required />
-              <i className="bx bxs-user"></i>
+              {/* <input type="text" placeholder="Email" required /> */}
+              <Input className="input-input" placeholder="Email" required />
+              <MailOutlined className="input-icon" />
             </div>
             <div className="input-box">
-              <input type="password" placeholder="Password" required />
-              <i className="bx bxs-lock-alt"></i>
+              {/* <input type="password" placeholder="Password" required /> */}
+              <Input.Password className="input-input" placeholder="Password" required />
+              <LockOutlined className="input-icon" />
             </div>
             <div className="forgot-link">
               <a href="#">Forgot Password?</a>
@@ -150,10 +198,10 @@ function Login() {
             <button type="submit" className="btn">Login</button>
             <p>or login with social platforms</p>
             <div className="social-icons">
-              <a href="#"><i className="bx bxl-google"></i></a>
-              <a href="#"><i className="bx bxl-facebook"></i></a>
-              <a href="#"><i className="bx bxl-github"></i></a>
-              <a href="#"><i className="bx bxl-linkedin"></i></a>
+              <a href="#"><GoogleOutlined /></a>
+              <a href="#"><FacebookOutlined /></a>
+              <a href="#"><GithubOutlined /></a>
+              <a href="#"><LinkedinOutlined /></a>
             </div>
           </form>
         </div>
@@ -161,41 +209,45 @@ function Login() {
         <div className="form-box register">
           <form action="">
             <h1>Registration</h1>
-            <div className="input-box">
-              <input type="text" placeholder="Username" required />
-              <i className="bx bxs-user"></i>
+            <div className="reg-input-box">
+              {/* <input type="text" placeholder="Username" required /> */}
+              <Input className="input-input" placeholder="Username" required />
+              <UserOutlined className="input-icon" />
             </div>
-            <div className="input-box">
-              <input type="email" placeholder="Email" required />
-              <i className="bx bxs-envelope"></i>
+            <div className="reg-input-box">
+              <Input className="input-input" placeholder="Email" required />
+              <MailOutlined className="input-icon" />
             </div>
-            <div className="input-box">
-              <input type="password" placeholder="Password" required />
-              <i className="bx bxs-lock-alt"></i>
+            <div className="reg-input-box">
+              <Input className="code-input-input" placeholder="Verification code" required maxLength={6} />
+              <a className="send-code" onClick={() => handleSendEmailCode()}>
+                {isShowCode ? `Resend in ${time}s` : 'Send Code'}
+              </a>
             </div>
-            <div className="forgot-link">
-              <a href="#">Forgot Password?</a>
+            <div className="reg-input-box">
+              <Input.Password className="input-input" placeholder="Password" required />
+              <LockOutlined className="input-icon" />
             </div>
             <button type="submit" className="btn">Register</button>
             <p>or register with social platforms</p>
             <div className="social-icons">
-              <a href="#"><i className="bx bxl-google"></i></a>
-              <a href="#"><i className="bx bxl-facebook"></i></a>
-              <a href="#"><i className="bx bxl-github"></i></a>
-              <a href="#"><i className="bx bxl-linkedin"></i></a>
+              <a href="#"><GoogleOutlined /></a>
+              <a href="#"><FacebookOutlined /></a>
+              <a href="#"><GithubOutlined /></a>
+              <a href="#"><LinkedinOutlined /></a>
             </div>
           </form>
         </div>
 
         <div className="toggle-box">
           <div className="toggle-panel toggle-left">
-            <h1>Hello, Welcome!</h1>
+            <h1>Welcome Back!</h1>
             <p>Don't have an account?</p>
             <button className="btn register-btn" onClick={handleRegisterClick}>Register</button>
           </div>
 
           <div className="toggle-panel toggle-right">
-            <h1>Welcome Back!</h1>
+            <h1>Hello, Welcome!</h1>
             <p>Already have an account?</p>
             <button className="btn login-btn" onClick={handleLoginClick}>Login</button>
           </div>
