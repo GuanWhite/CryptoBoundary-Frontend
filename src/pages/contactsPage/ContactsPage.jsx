80075a3f71140800f24ffba2c14ed1cbef1ed203
white <3488337866@qq.com>
2025-05-07 23:52:39 +0800

    Developing contact cards for the contact page

--- src/pages/contactsPage/ContactsPage.jsx
+++ src/pages/contactsPage/ContactsPage.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import ContactListCard from '../../components/ContactListCard/ContactListCard';
+import ChatRoomPage from '../chatRoomPage/ChatRoomPage';
 
 export default function ContactsPage() {
   return (
-    <div>ContactsPage</div>
+    <div className='min-h-screen w-full flex'>
+      <div className="pt-[32px] h-full bg-[#e5e5e5]">
+        <ContactListCard />
+        <ContactListCard />
+        <ContactListCard />
+        <ContactListCard />
+        <ContactListCard />
+        <ContactListCard />
+        <ContactListCard />
+        <ContactListCard />
+        <ContactListCard />
+        <ContactListCard />
+        <div className="w-72 mt-[1.5em] pt-[1.2em] border-t-[0.15em] border-dashed border-[rgba(0,0,0,0.15)]"/>
+      </div>
+      <ChatRoomPage />
+    </div>
   );
 }
